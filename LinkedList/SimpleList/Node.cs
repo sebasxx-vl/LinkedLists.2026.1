902df5dc48013fb2ae245693074a85f7dec0483c
Sebastian López <sebasgal608@gmail.com>
2026-04-24 22:30:22 -0500

    Simple List and Node

---
+++ LinkedList/SimpleList/Node.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleList;
+public class Node<T>
+{
+
+    public Node(T? data)
+    {
+        Data = data;
+        Next = null;
+    }
+    public T? Data { get; set; }
+    public Node<T>? Next { get; set; }
+}
