35eceb98438ab71d0c2f79e4fe33e1e829aadc86
Sebastian López <sebasgal608@gmail.com>
2026-04-24 22:44:20 -0500

    Complete

---
+++ LinkedList/DoubleList/DoubleLinkedList.cs
@@ -0,0 +1,121 @@
+﻿using Shared;
+
+namespace DoubleList;
+
+public class DoubleLinkedList<T> : ILinkedList<T>
+{
+    private Node<T>? _head;
+    private Node<T>? _tail;
+
+    public DoubleLinkedList()
+    {
+        _head = null;
+        _tail = null;
+    }
+
+    public bool Contains(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new Node<T>(data);
+        if (_head == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Previous = newNode;
+            _head = newNode;
+        }
+    }
+
+    public void InsertAtEnding(T data)
+    {
+        var newNode = new Node<T>(data);
+        if (_tail == null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        else
+        {
+            _tail.Next = newNode;
+            newNode.Previous = _tail;
+            _tail = newNode;
+        }
+    }
+
+    public void InsertOrdered(T data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(T data)
+    {
+        var current = _head;
+        while (current != null)
+        {
+            if (current.Data!.Equals(data))
+            {
+                if (current == _head) // Found at the head
+                {
+                    _head = _head.Next;
+                    _head!.Previous = null;
+                }
+                else if (current == _tail) // Found at the tail
+                {
+                    _tail = _tail.Previous;
+                    _tail!.Next = null;
+                }
+                else // Found in the middle
+                {
+                    current.Previous!.Next = current.Next;
+                    current.Next!.Previous = current.Previous;
+                }
+                return;
+            }
+            current = current.Next;
+        }
+    }
+
+    public void Reverse()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Sort()
+    {
+        throw new NotImplementedException();
+    }
+
+    override public string ToString()
+    {
+        var current = _head;
+        var result = string.Empty;
+        while (current != null)
+        {
+            result += $"{current.Data} -> ";
+            current = current.Next;
+        }
+        result += "null";
+        return result;
+    }
+
+    public string ToStringReverse()
+    {
+        var current = _tail;
+        var result = string.Empty;
+        while (current != null)
+        {
+            result += $"{current.Data} -> ";
+            current = current.Previous;
+        }
+        result += "null";
+        return result;
+    }
+}
---
+++ LinkedList/Shared/ILinkedList.cs
@@ -0,0 +1,13 @@
+﻿namespace Shared;
+
+public interface ILinkedList<T>
+{
+    bool Contains(T data);
+    void InsertAtBeginning(T data);
+    void InsertAtEnding(T data);
+    void InsertOrdered(T data);
+    void Remove(T data);
+    void Reverse();
+    void Sort();
+    string ToString();
+}
